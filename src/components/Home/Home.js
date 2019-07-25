097f3b8b3c0482b23658b892393a9ca4a8bf2176
enlightened-havok <erik_manley@hotmail.com>
2019-07-24 20:56:05 -0600

    Just committing a few comments for Erik

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './home.css';
 
 class Home extends Component {
+  
+  //runs when the code starts
+  componentDidMount(){
+    //this.function();
+  }
+
+  //initialize functions here
+
   render () {
     return (
       <div className="home-container">
@@ -18,5 +26,7 @@ class Home extends Component {
     )
   }
 }
+
+
 
 export default Home
